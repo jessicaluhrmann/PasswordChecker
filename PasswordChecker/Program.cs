fb021cac667d53f9ff5173109c12d06b6cee5c75
jessicaluhrmann <jessica.luhrmann@xplortechnologies.com>
2023-10-17 15:00:05 +1000

    Add project files.

---
+++ PasswordChecker/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var minLength = 8;
+            var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var lowercase = "abcdefghijklmnopqrstuvwxyz";
+            var digits = "0123456789";
+            var specialChars = "!@#$%^&*()-_+=?/\\.,><~;:\'\"";
+            var score = 0;
+
+            Console.WriteLine("Enter a password:");
+            var password = Console.ReadLine();
+
+            if (password.Length >= minLength)
+            {
+                score++;
+            };
+
+            if (Tools.Contains(password, uppercase))
+            {
+                score++;
+            };
+
+            if (Tools.Contains(password, lowercase))
+            {
+                score++;
+            };
+
+            if (Tools.Contains(password, digits))
+            {
+                score++;
+            };
+
+            if (Tools.Contains(password, specialChars))
+            {
+                score++;
+            };
+
+
+
+            switch (score)
+            {
+                case 5:
+                case 4:
+                    Console.WriteLine("Your password is extremely strong");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Your password is strong");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Your password is medium");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Your password is weak");
+                    break;
+
+                default:
+                    Console.WriteLine("Your password doesn't meet any of the standards");
+                    break;
+            }
+        }
+    }
+}
